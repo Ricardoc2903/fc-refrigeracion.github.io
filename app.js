22afd89c53f949b4e594ce5e8004eff15d7914f2
Ricardo Coello <ricardodavidquevedo29@gmail.com>
2024-01-28 16:51:29 -0300

    añadiendo try catch al metodo para enviar el form

--- app.js
+++ app.js
@@ -16,24 +16,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); // Asegúrate de que se envíe el archivo correcto
 });
 
-// Ruta para manejar los datos del formulario
-app.post('/enviar-formulario', (req, res) => {
-  const { nombre, email, telefono, mensaje, quieroOfertas } = req.body;
+app.post('/enviar-formulario', async (req, res) => {
+  try {
+    const { nombre, email, telefono, mensaje, quieroOfertas } = req.body;
 
-  // Validar y procesar los datos (aquí deberías hacer más validaciones según tus necesidades)
+    // Insertar datos en la base de datos
+    const sql = 'INSERT INTO formulario (nombre, email, telefono, mensaje, quiere_ofertas) VALUES (?, ?, ?, ?, ?)';
+    await db.query(sql, [nombre, email, telefono, mensaje, quieroOfertas]);
 
-  // Insertar datos en la base de datos
-  const sql = 'INSERT INTO formulario (nombre, email, telefono, mensaje, quiere_ofertas) VALUES (?, ?, ?, ?, ?)';
-  db.query(sql, [nombre, email, telefono, mensaje, quieroOfertas], (err, result) => {
-    if (err) {
-      console.error('Error al insertar datos en la base de datos: ', err);
-      res.status(500).json({ success: false, message: 'Error en el servidor' });
-    } else {
-      console.log('Datos insertados correctamente en la base de datos');
-      res.status(200).json({ success: true, message: 'Datos enviados con éxito' });
-    }
-  });
+    console.log('Datos insertados correctamente en la base de datos');
+    res.status(200).json({ success: true, message: 'Datos enviados con éxito' });
+  } catch (error) {
+    console.error('Error al insertar datos en la base de datos: ', error);
+    res.status(500).json({ success: false, message: 'Error en el servidor' });
+  }
 });
+
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
