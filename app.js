e15e3466c7f19fccef7ba2b365c10b0526329a20
Ricardo Coello <ricardodavidquevedo29@gmail.com>
2024-01-28 15:57:29 -0300

    cambiando rutas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.send('index.html');
 });
 
 // Ruta para manejar los datos del formulario
