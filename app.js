601b43ac0d76aa512de2e5fa0ee1431bd86f03c1
Ricardo Coello <ricardodavidquevedo29@gmail.com>
2024-02-01 19:40:39 -0300

    probando configuracion de cors para el form

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const db = require('./db');  // Importa la configuración de la base de datos
 const cors = require('cors');
+const path = require('path');
+const db = require('./db');
 
 const app = express();
 const port = 3000;
 
-app.use(cors()); // Coloca esto antes de las demás configuraciones de middleware
+const corsOptions = {
+  origin: 'https://fc-refrigeracion-github-aye4oa03b-ricardoc2903s-projects.vercel.app', // Replace with your client's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Asegúrate de que se envíe el archivo correcto
+  res.send('index.html');
 });
 
-app.post('/enviar-formulario', async (req, res) => {
-  try {
-    const { nombre, email, telefono, mensaje, quieroOfertas } = req.body;
+app.post('/enviar-formulario', (req, res) => {
+  const { nombre, email, telefono, mensaje } = req.body;
 
-    // Insertar datos en la base de datos
-    const sql = 'INSERT INTO formulario (nombre, email, telefono, mensaje, quiere_ofertas) VALUES (?, ?, ?, ?, ?)';
-    await db.query(sql, [nombre, email, telefono, mensaje, quieroOfertas]);
-
-    console.log('Datos insertados correctamente en la base de datos');
-    res.status(200).json({ success: true, message: 'Datos enviados con éxito' });
-  } catch (error) {
-    console.error('Error al insertar datos en la base de datos: ', error);
-    res.status(500).json({ success: false, message: 'Error en el servidor' });
-  }
+  const sql = 'INSERT INTO formulario (nombre, email, telefono, mensaje) VALUES (?, ?, ?, ?)';
+  db.query(sql, [nombre, email, telefono, mensaje], (err, result) => {
+    if (err) {
+      console.error('Error al insertar datos en la base de datos: ', err);
+      res.status(500).json({ success: false, message: 'Error en el servidor' });
+    } else {
+      console.log('Datos insertados correctamente en la base de datos');
+      res.status(200).json({ success: true, message: 'Datos enviados con éxito' });
+    }
+  });
 });
-
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
