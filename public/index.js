a3d30d6e4aac3eb5b02ab9bfe648e3b9dd95c66d
Ricardo Coello <ricardodavidquevedo29@gmail.com>
2024-02-10 17:28:46 -0300

    22

--- public/index.js
+++ public/index.js
@@ -38,38 +38,33 @@ $(document).ready(function () {
   })
 
 // Captura el evento de envío del formulario
-$("#miFormulario").submit(function (e) {
-    console.log("Formulario enviado");
-    e.preventDefault(); // Evita el envío normal del formulario
+$(document).ready(function () {
+  console.log("Documento listo");
+  // Captura el evento de envío del formulario
+  $("#miFormulario").submit(function (e) {
+      console.log("Formulario enviado");
+      e.preventDefault(); // Evita el envío normal del formulario
 
-    $(document).ready(function () {
-      console.log("Documento listo");
-      // Captura el evento de envío del formulario
-      $("#miFormulario").submit(function (e) {
-          console.log("Formulario enviado");
-          e.preventDefault(); // Evita el envío normal del formulario
-  
-          // Obtén los datos del formulario y crea un objeto FormData
-          var formData = new FormData($(this)[0]);
-  
-          // Realiza una petición AJAX para enviar el formulario
-          $.ajax({
-              type: "POST",
-              url: "/enviar-formulario", // La URL de tu ruta que maneja el formulario
-              data: formData,
-              processData: false,
-              contentType: false,
-              success: function (response) {
-                  // Actualiza el contenido del span con el mensaje de resultado
-                  console.log("Éxito:", response);
-                  $("#message").text(response.message);
-              },
-              error: function (xhr, status, error) {
-                  // En caso de error, muestra el mensaje de error específico en la consola
-                  console.log("Error al enviar el formulario:", xhr.responseText);
-                  $("#message").text("Error al enviar el formulario: " + xhr.responseText);
-              },
-          });
+      // Obtén los datos del formulario y crea un objeto FormData
+      var formData = new FormData($(this)[0]);
+
+      // Realiza una petición AJAX para enviar el formulario
+      $.ajax({
+          type: "POST",
+          url: "/enviar-formulario", // La URL de tu ruta que maneja el formulario
+          data: formData,
+          processData: false,
+          contentType: false,
+          success: function (response) {
+              // Actualiza el contenido del span con el mensaje de resultado
+              console.log("Éxito:", response);
+              $("#message").text(response.message);
+          },
+          error: function (xhr, status, error) {
+              // En caso de error, muestra el mensaje de error específico en la consola
+              console.log("Error al enviar el formulario:", xhr.responseText);
+              $("#message").text("Error al enviar el formulario: " + xhr.responseText);
+          },
       });
   });
-  
+});
